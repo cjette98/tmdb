cfabb22098eb39cb251cfcabd4e663cf28f46899
JasperIra12 <jasperiracastillo12@gmail.com>
2022-08-22 21:52:38 +0800

    feat/fetching_movie_list

--- src/service/index.js
+++ src/service/index.js
@@ -1,27 +1,33 @@
-const CATEGORY = [
-    {
-        id: '1',
-        cat_name: 'Horror'
-    }
-];
+const base_url = "https://api.themoviedb.org"
+const API_KEY = "?api_key=5a189ff34bd9b38eb4b94fa7caf9dabb"
 
-const dummyMovie = [
-    {
-        id:1,
-        title:'18 years old',
-        catId:1,
-        image:',png'
-    },
-    {
-        id:1,
-        title:'18 years old',
-        catId:1,
-        image:',png'
-    },
-    {
-        id:1,
-        title:'18 years old',
-        catId:1,
-        image:',png'
-    }
-]
+async function postGetMovieDetails(url = '') {
+    // Default options are marked with *
+    const response = await fetch(`${base_url}${url}${API_KEY}`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    return response.json(); 
+  }
+
+  async function GetMovieList(url = '') {
+    // Default options are marked with *
+    const response = await fetch(`${base_url}${url}${API_KEY}`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    return response.json(); 
+  }
+
+
+  export {
+    postGetMovieDetails,
+    GetMovieList
+
+  }
